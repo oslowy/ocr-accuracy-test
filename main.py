891a459b8be26c337ae3f2b1370b72cd0ff26828
Orion Lowy <oslowy@uh.edu>
2021-11-10 18:38:28 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import json
+import xml.dom.minidom as dom
+
+
+def extract_image_info(image_element):
+    tagged_rectangle_elements = image_element.getElementsByTagName("taggedRectangle")
+
+    return {e.getElementsByTagName("tag")[0]: dict(e.attributes.items())
+            for e in tagged_rectangle_elements}
+
+
+def ground_truth_dictionary(gt_xml_file):
+    document = dom.parse(gt_xml_file)
+    image_elements = document.getElementsByTagName("image")
+
+    return {e.getElementsByTagName("imageName")[0]: extract_image_info(e)
+            for e in image_elements}
+
+
+def main():
+    args = sys.argv[1:]
+
+    with open(args[0]) as gt_xml_file:
+        print(ground_truth_dictionary(gt_xml_file))
+
+
+if __name__ == "__main__":
+    main()
