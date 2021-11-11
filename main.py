258e686daf5938cfd67f3df1c6d0f98f1dd190af
Orion Lowy <oslowy@uh.edu>
2021-11-11 10:45:45 -0600

    Consistent dictionary key format

--- main.py
+++ main.py
@@ -6,16 +6,18 @@ import xml.dom.minidom as dom
 def extract_image_info(image_element):
     tagged_rectangle_elements = image_element.getElementsByTagName("taggedRectangle")
 
-    return {e.getElementsByTagName("tag")[0].firstChild.data: dict(e.attributes.items())
-            for e in tagged_rectangle_elements}
+    return [dict([('word', e.getElementsByTagName("tag")[0].firstChild.data)]
+                 + e.attributes.items())
+            for e in tagged_rectangle_elements]
 
 
 def ground_truth_dictionary(gt_xml_file):
     document = dom.parse(gt_xml_file)
     image_elements = document.getElementsByTagName("image")
 
-    return {e.getElementsByTagName("imageName")[0].firstChild.data: extract_image_info(e)
-            for e in image_elements}
+    return [dict([('image', e.getElementsByTagName("imageName")[0].firstChild.data),
+                  ('words', extract_image_info(e))])
+            for e in image_elements]
 
 
 def main():
